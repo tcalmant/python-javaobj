a6d89b707bde4c1cc9bdc921ffc890293b79d687
vbuell <vbuell@003243c8-0679-609b-391f-34f4f14b4f20>
2011-03-29 06:15:01 +0000

    setup.py added

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+setup(
+    name="javaobj",
+    version="0.1.0",
+    author="Volodymyr Buell",
+    author_email="vbuell@gmail.com",
+    url="http://code.google.com/p/python-javaobj",
+    description=("Module for serializing and de-serializing Java objects."),
+    license="APL2",
+    keywords="python java marshalling serialization",
+#    packages=['javaobj'],
+    long_description="Provides functions for reading and writing (writing is WIP currently) " \
+                     "Java objects serialized or will be deserialized by ObjectOutputStream. " \
+                     "This form of object representation is a standard data interchange format " \
+                     "in Java world. javaobj module exposes an API familiar to users of the " \
+                     "standard library marshal, pickle and json modules.",
+    classifiers=[
+            "Development Status :: 3 - Alpha",
+            "License :: OSI Approved :: Apache Software License",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            ],
+    )
