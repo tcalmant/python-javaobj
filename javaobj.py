5f90f15b40f61aa778008cfc816ced3ee1264617
Thomas Calmant <thomas.calmant@isandlatech.com>
2013-12-23 11:45:11 +0100

    Added APLv2 header to javaobj.py

--- javaobj.py
+++ javaobj.py
@@ -13,6 +13,22 @@ http://download.oracle.com/javase/6/docs/platform/serialization/spec/protocol.ht
 :license: Apache License 2.0
 :version: 0.1.1
 :status: Alpha
+
+..
+
+    Copyright 2013 Thomas Calmant
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 """
 
 # Module version
