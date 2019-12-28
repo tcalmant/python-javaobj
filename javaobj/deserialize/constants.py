5a55251fd98e769449132eb6a333bd95ea9b4acb
Thomas Calmant <thomas.calmant@gmail.com>
2019-12-28 14:38:26 +0100

    Added missing license in module pydoc

--- javaobj/deserialize/constants.py
+++ javaobj/deserialize/constants.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 """
-Definition of the constants used in javaobj
+Definition of the constants used in the deserialization process
+
+:authors: Thomas Calmant
+:license: Apache License 2.0
+:version: 0.4.0
+:status: Alpha
+
+..
+
+    Copyright 2019 Thomas Calmant
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 """
 
 STREAM_MAGIC = 0xACED
--- javaobj/deserialize/core.py
+++ javaobj/deserialize/core.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 """
-New core version of python-javaobj, using the same approach as jdeserialize
+Second parsing approach for javaobj, using the same approach as jdeserialize
+See: https://github.com/frohoff/jdeserialize
+
+:authors: Thomas Calmant
+:license: Apache License 2.0
+:version: 0.4.0
+:status: Alpha
+
+..
+
+    Copyright 2019 Thomas Calmant
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 """
 
 from enum import Enum
